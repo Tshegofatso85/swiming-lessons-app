782bc609dba65cf1ccd7f1bd0bfbd5d13688f34c
Tshegofatso85 <generalmosha85@gmail.com>
2026-03-19 17:12:40 +0200

    refactored code

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,9 +1,9 @@
 async function loadLayout() {
-  const navbar = await fetch("../components/navbar.html");
+  const navbar = await fetch("./components/navbar.html");
   const navbarHTML = await navbar.text();
   document.getElementById("navbar").innerHTML = navbarHTML;
 
-  const footer = await fetch("../components/footer.html");
+  const footer = await fetch("./components/footer.html");
   const footerHTML = await footer.text();
   document.getElementById("footer").innerHTML = footerHTML;
 
